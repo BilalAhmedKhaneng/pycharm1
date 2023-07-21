cc0cbbd6796c6283e4c71d9262f9bf31bc7716d8
bilal <bilalahmedkhan.eng@gmail.com>
2023-07-21 21:09:53 +0500

    recursion f statemnt

---
+++ f  strings formating.py
@@ -0,0 +1,7 @@
+#letter = "my name is {1} and i am from {0}"
+#country="pakistan"
+#name="bilal"
+
+print(letter.format(country,name))
+#now using f statment
+print(f"my name is {name}and i am from {country}")
