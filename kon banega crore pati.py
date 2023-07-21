647268b5e66aae8da158ad241017ad19d4f4d338
bilal <bilalahmedkhan.eng@gmail.com>
2023-07-21 17:23:33 +0500

    list method

---
+++ kon banega crore pati.py
@@ -0,0 +1,12 @@
+#use list data type to store questions and their correct answers
+#dispplay teh final amount
+age = int(input())
+print(age,'your age is')
+if (age<10):
+    print("your adult")
+elif(age>=10):
+ print("you r low")
+if (age>=20):
+    print("you are mature ")
+elif(age<=20):
+    ("you have to wait")
